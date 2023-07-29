2b4fced46559844b2aa7ba43207843cb86c1aa43
manchuriqbal <manchur.fiverr@gmail.com>
2023-07-29 10:17:17 +0600

    i romove some thing from project

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from .tasks import notify_customer
+
 
 
 def say_hello(request):
-    notify_customer.delay("Hello!")
    
     return render(request, 'hello.html', {'name': 'Mosh'})
